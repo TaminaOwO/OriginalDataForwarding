253c00a791bce21ad0b5463dbb024508c3cf9a8d
Patty <patty@cmoney.net.tw>
2017-11-20 10:25:04 +0800

    TcpClient 加上Send and Receive buffer size

--- Modules/TCPListener/ProxyServer.cs
+++ Modules/TCPListener/ProxyServer.cs
@@ -18,52 +18,52 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// </summary>
         /// <param name="socketPort"></param>
         /// <param name="outputMessage">輸出訊息</param>
-        public ProxyServer ( int socketPort, Action<string> outputMessage)
+        public ProxyServer ( int socketPort , Action<string> outputMessage )
         {
             fOutputMessage = outputMessage;
 
             // Listener worker
-            fTcpListener = new TcpListener( IPAddress.Any, socketPort );
-            fToken = new CancellationTokenSource();
+            fTcpListener = new TcpListener ( IPAddress.Any , socketPort );
+            fToken = new CancellationTokenSource ( );
 
             //事件處理
-            OnStatus = new ThreadEvent<string>();
-            OnData = new ThreadEvent<string>();
-            OnConnectionStatus = new ThreadEvent<ProxyClient>();
+            OnStatus = new ThreadEvent<string> ( );
+            OnData = new ThreadEvent<string> ( );
+            OnConnectionStatus = new ThreadEvent<ProxyClient> ( );
 
             // 平行結構
-            fClientTmpQueue = new ThreadBridge<ProxyClient>();
-            fCastingMessage = new ThreadBridge<AckTask>();  
+            fClientTmpQueue = new ThreadBridge<ProxyClient> ( );
+            fCastingMessage = new ThreadBridge<AckTask> ( );
 
             // connection pool
-            fClientPool = new List<ProxyClient>();
+            fClientPool = new List<ProxyClient> ( );
 
             //connection worker
-            fTaskListener = Task.Factory.StartNew( ListenerAsync, fToken.Token );
+            fTaskListener = Task.Factory.StartNew ( ListenerAsync , fToken.Token );
 
             // boradcastor
-            fBroadcastor = Task.Factory.StartNew( BroadcastingAsync, fToken.Token );
+            fBroadcastor = Task.Factory.StartNew ( BroadcastingAsync , fToken.Token );
         }
-        ~ProxyServer()
+        ~ProxyServer ( )
         {
-            Dispose();
+            Dispose ( );
         }
 
         /// <summary>
         /// 釋放資源，放開執行緒
         /// </summary>
-        public void Dispose()
+        public void Dispose ( )
         {
             // 請求停止執行緒
-            fToken.Cancel();
+            fToken.Cancel ( );
 
             // 停止listener
-            fTcpListener.Stop();
+            fTcpListener.Stop ( );
 
             // 等待停止
-            fTaskListener.Wait();
+            fTaskListener.Wait ( );
 
-            GC.SuppressFinalize( this );
+            GC.SuppressFinalize ( this );
         }
 
         /// <summary>
@@ -83,7 +83,7 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// </summary>
         private CancellationTokenSource fToken;
 
-        private byte[] fReadByte = new byte[4194304];
+        private byte[ ] fReadByte = new byte[ 4194304 ];
 
         /// <summary>
         /// listener
@@ -142,48 +142,51 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 接收client 連線
         /// </summary>
         /// <param name="tokenObj"></param>
-        private void ListenerAsync( object tokenObj )
+        private void ListenerAsync ( object tokenObj )
         {
-            CancellationToken token = (CancellationToken)tokenObj;
+            CancellationToken token = ( CancellationToken ) tokenObj;
 
             //啟動接收 10個連線數
-            fTcpListener.Start(10);
+            fTcpListener.Start ( 10 );
 
             //端點資訊
-            var info = (IPEndPoint)fTcpListener.Server.LocalEndPoint;
+            var info = ( IPEndPoint ) fTcpListener.Server.LocalEndPoint;
 
             //開始工作囉!
             while ( !fToken.IsCancellationRequested )
             {
                 try
                 {
-                    OnStatus.OnFireMessage( string.Format( "[Server] 等待Client連線...Port:{0}", info.Port ) );
+                    OnStatus.OnFireMessage ( string.Format ( "[Server] 等待Client連線...Port:{0}" , info.Port ) );
 
                     //執行續會所在這，一直到有新的連線才會繼續走
-                    TcpClient client = fTcpListener.AcceptTcpClient();
+                    TcpClient client = fTcpListener.AcceptTcpClient ( );
+                    client.NoDelay = true;
+                    client.SendBufferSize = 1024 * 1024 ;
+                    client.ReceiveBufferSize = 1024 * 1024;
 
                     //丟到平行資料結構
                     //這個queue的進出要很快，才不會影響到連線效率
-                    var currentClient = new ProxyClient( client );
-                    fClientTmpQueue.Enqueue( currentClient );
+                    var currentClient = new ProxyClient ( client );
+                    fClientTmpQueue.Enqueue ( currentClient );
 
-                    OnStatus.OnFireMessage( string.Format( "[Clinet] 來自{0}的連線...Port:{1}", currentClient.Address, info.Port ) );
+                    OnStatus.OnFireMessage ( string.Format ( "[Clinet] 來自{0}的連線...Port:{1}" , currentClient.Address , info.Port ) );
 
                     // log
-                    OnConnectionStatus.OnFireMessage( currentClient );
+                    OnConnectionStatus.OnFireMessage ( currentClient );
                 }
                 catch ( SocketException se )
                 {
-                    OnStatus.OnFireMessage( string.Format( "SocketCode:{0}\r\n{1}", se.SocketErrorCode, se.ToString() ) );
+                    OnStatus.OnFireMessage ( string.Format ( "SocketCode:{0}\r\n{1}" , se.SocketErrorCode , se.ToString ( ) ) );
                 }
                 catch ( Exception e )
                 {
-                    OnStatus.OnFireMessage( e.ToString() );
+                    OnStatus.OnFireMessage ( e.ToString ( ) );
                 }
             }
 
             //Request 取消
-            token.ThrowIfCancellationRequested();
+            token.ThrowIfCancellationRequested ( );
         }
 
 
@@ -191,9 +194,9 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 處理廣播訊息
         /// </summary>
         /// <param name="tokenObj"></param>
-        private void BroadcastingAsync( object tokenObj )
+        private void BroadcastingAsync ( object tokenObj )
         {
-            CancellationToken token = (CancellationToken)tokenObj;
+            CancellationToken token = ( CancellationToken ) tokenObj;
 
             //開始工作囉!
             while ( !fToken.IsCancellationRequested )
@@ -201,27 +204,27 @@ namespace OriginalDataForwarding.Modules.TCPListener
                 try
                 {
                     //管理連線池
-                    DumpAndThrowDeadClient();
+                    DumpAndThrowDeadClient ( );
 
                     //拿出要處理的工作
-                    var allPacketage = fCastingMessage.DequeueAll().ToArray();
+                    var allPacketage = fCastingMessage.DequeueAll ( ).ToArray ( );
                     if ( allPacketage.Length == 0 )
                     {
-                        Thread.Sleep( 10 );
+                        Thread.Sleep ( 10 );
                     }
                     else
                     {
                         //廣播
-                        SendBroadcasting( allPacketage );
+                        SendBroadcasting ( allPacketage );
                     }
                 }
                 catch ( Exception e )
                 {
-                    OnStatus.OnFireMessage( e.ToString() );
+                    OnStatus.OnFireMessage ( e.ToString ( ) );
                 }
             }
 
-            token.ThrowIfCancellationRequested();
+            token.ThrowIfCancellationRequested ( );
         }
 
         #endregion
@@ -232,11 +235,11 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 廣播訊息
         /// </summary>
         /// <param name="message"></param>
-        public void Broadcasting( byte[] pkg, ushort dataType )
+        public void Broadcasting ( byte[ ] pkg , ushort dataType )
         {
-            fCastingMessage.Enqueue( new AckTask()
+            fCastingMessage.Enqueue ( new AckTask ( )
             {
-                DataBytes = pkg,
+                DataBytes = pkg ,
                 DataType = dataType
             } );
         }
@@ -246,7 +249,7 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 取得有效的連線數
         /// </summary>
         /// <returns></returns>
-        public int GetAvailableClinetCount()
+        public int GetAvailableClinetCount ( )
         {
             return fClientPool.Count;
         }
@@ -255,7 +258,7 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 取得有效的連線數
         /// </summary>
         /// <returns></returns>
-        public List<ProxyClient> GetAvailableClinets()
+        public List<ProxyClient> GetAvailableClinets ( )
         {
             return fClientPool;
         }
@@ -264,16 +267,16 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 踢出指定連線
         /// </summary>
         /// <param name="willRemoveClients">欲剔除的連線</param>
-        public void RemoveClients( List<ProxyClient> willRemoveClients )
+        public void RemoveClients ( List<ProxyClient> willRemoveClients )
         {
             foreach ( var item in willRemoveClients )
             {
                 // 紀錄斷線 LOG
-                OnConnectionStatus.OnFireMessage( item );
+                OnConnectionStatus.OnFireMessage ( item );
 
-                fClientPool.Remove( item );
+                fClientPool.Remove ( item );
 
-                OnStatus.OnFireMessage( string.Format( "剔除 Client [{0}]", item.Address ) );
+                OnStatus.OnFireMessage ( string.Format ( "剔除 Client [{0}]" , item.Address ) );
             }
         }
 
@@ -282,32 +285,32 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// <summary>
         /// 丟掉斷開連線的Client
         /// </summary>
-        private void DumpAndThrowDeadClient()
+        private void DumpAndThrowDeadClient ( )
         {
             // 拿出連線的
-            var allConnection = fClientTmpQueue.DequeueAll();
+            var allConnection = fClientTmpQueue.DequeueAll ( );
             if ( allConnection.Count != 0 )
             {
-                fClientPool.AddRange( allConnection );
+                fClientPool.AddRange ( allConnection );
             }
 
             // 超過限制數量的連線
-            var overLimitClients = fClientPool.Where( x => x.ClientSocket.Connected)
-                                    .GroupBy( x => x.Address ).ToDictionary( x => x.Key, x => x.ToList() )
-                                    .Where( x => x.Value.Count > SAME_IP_LIMIT_COUNT )
-                                    .Select( x => x.Value.LastOrDefault() ).ToList();
+            var overLimitClients = fClientPool.Where ( x => x.ClientSocket.Connected )
+                                    .GroupBy ( x => x.Address ).ToDictionary ( x => x.Key , x => x.ToList ( ) )
+                                    .Where ( x => x.Value.Count > SAME_IP_LIMIT_COUNT )
+                                    .Select ( x => x.Value.LastOrDefault ( ) ).ToList ( );
 
             // 已斷線的連線
-            var disconnected = fClientPool.Where( x => !x.ClientSocket.Connected ).ToList();
+            var disconnected = fClientPool.Where ( x => !x.ClientSocket.Connected ).ToList ( );
 
             // 丟掉不要的連線
-            foreach ( var item in disconnected.Concat(overLimitClients).Distinct() )
+            foreach ( var item in disconnected.Concat ( overLimitClients ).Distinct ( ) )
             {
                 // 紀錄斷線LOG
-                OnConnectionStatus.OnFireMessage( item );
+                OnConnectionStatus.OnFireMessage ( item );
 
-                fClientPool.Remove( item );
-                OnStatus.OnFireMessage( string.Format( "Clinet斷線[{0}]", item.Address ) );
+                fClientPool.Remove ( item );
+                OnStatus.OnFireMessage ( string.Format ( "Clinet斷線[{0}]" , item.Address ) );
             }
         }
 
@@ -315,10 +318,10 @@ namespace OriginalDataForwarding.Modules.TCPListener
         /// 送出通知工作
         /// </summary>
         /// <param name="successCount"></param>
-        private void SendBroadcasting( params AckTask[] messages )
+        private void SendBroadcasting ( params AckTask[ ] messages )
         {
             var taskStartTick = Environment.TickCount;
-            var usedTicks = new List<string>();
+            var usedTicks = new List<string> ( );
 
             //打出通知
             int total = 0;
@@ -339,38 +342,36 @@ namespace OriginalDataForwarding.Modules.TCPListener
                             total++;
 
                             int writeTick = Environment.TickCount;
-                            
+
                             //發送資料
                             if ( item.ClientStream.CanWrite )
                             {
-                                item.ClientStream.Write( data.DataBytes, 0, data.DataBytes.Length );
+                                item.ClientStream.Write ( data.DataBytes , 0 , data.DataBytes.Length );
 
                                 //ACK
                                 success++;
                             }
 
                             writeTick = Environment.TickCount - writeTick;
-                            int readTick = Environment.TickCount;
 
                             //處理讀取資料(略過不做)
                             if ( item.ClientStream.CanRead && item.ClientStream.DataAvailable )
                             {
                                 //丟掉回傳資料
-                                item.ClientStream.Read( fReadByte, 0, fReadByte.Length );
+                                item.ClientStream.Read ( fReadByte , 0 , fReadByte.Length );
                             }
 
-                            readTick = Environment.TickCount - readTick;
-                            tickMessage = string.Format( "W:{0},R:{1}", writeTick, readTick );
+                            tickMessage = string.Format ( "W:{0}" , writeTick );
                         }
                     }
                     catch ( Exception e )
                     {
-                        OnStatus.OnFireMessage( e.ToString() );
+                        OnStatus.OnFireMessage ( e.ToString ( ) );
                         exceptionCount++;
                     }
 
                     sendBroadcastingTick = Environment.TickCount - sendBroadcastingTick;
-                    usedTicks.Add( string.Format( "{0}[{1}]", sendBroadcastingTick, tickMessage ?? "Exception" ) );
+                    usedTicks.Add ( string.Format ( "{0}[{1}]" , sendBroadcastingTick , tickMessage ?? "Exception" ) );
                 }
             }
 
@@ -379,9 +380,9 @@ namespace OriginalDataForwarding.Modules.TCPListener
             //如果單次轉發超過300ms就應該注意
             if ( taskStartTick > 300 )
             {
-                fOutputMessage( string.Format( "SendBroadcasting TaskTick : {0}", taskStartTick ) );
-                fOutputMessage( string.Format( "TotalCount : {0} SuccessCount : {1} ExceptionCount : {2}", total, success, exceptionCount ) );
-                fOutputMessage( string.Format( "UsedTicksList : {0}", string.Join( " , ", usedTicks ) ) );
+                fOutputMessage ( string.Format ( "SendBroadcasting TaskTick : {0}" , taskStartTick ) );
+                fOutputMessage ( string.Format ( "TotalCount : {0} SuccessCount : {1} ExceptionCount : {2}" , total , success , exceptionCount ) );
+                fOutputMessage ( string.Format ( "UsedTicksList : {0}" , string.Join ( " , " , usedTicks ) ) );
             }
         }
 
