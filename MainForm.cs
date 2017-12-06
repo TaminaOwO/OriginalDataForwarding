295b5d88a1ab010f00e461fa59af59fbec8e9324
elvis <elvis@cmoney.net.tw>
2017-12-06 15:16:15 +0800

    移除無用參考.

--- MainForm.cs
+++ MainForm.cs
@@ -4,14 +4,9 @@ using OriginalDataForwarding.Modules.TCPListener;
 using OriginalDataForwarding.Properties;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OriginalDataForwarding
